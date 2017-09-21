073ee02628b7a9f7285520292ab456f35f374f6b
刘敏 <lmskater@users.noreply.github.com>
2017-09-21 20:55:25 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity implements LoginView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //xxxxx
+        //xxxxxaa
     }
 
     private void init() {
